ff49c93304fd492cbb1de299e082eba1ddd20c0e
Niklas Sheth <niklassheth@gmail.com>
2020-09-16 13:41:27 -0400

    Add parser

---
+++ main/parser.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import functools
+import operator
+import requests
+import sys
+import re
+
+first = lambda xs: functools.reduce(lambda x, y: x or y, xs, False)
+
+def parseFromURL(url):
+        page = requests.get(url)
+        return BeautifulSoup(page.text, 'lxml')
+
+url = sys.argv[1]
+soup = parseFromURL(url)
+matchMeta = re.compile('author')
+author = [soup.find('meta', attrs={'name':matchMeta})]
+matchAuthor = re.compile('author.*byline|byline.*author|author-name|authorName', re.IGNORECASE)
+author.append( soup.find(attrs={'class': matchAuthor}) )
+print(first(author))
